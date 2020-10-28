0828fc203ccc75f0f22549b2e9715f48c4ef86f5
Robert Allaway <allaway@users.noreply.github.com>
2020-10-28 10:28:31 -0700

    Update app.R

--- app.R
+++ app.R
@@ -591,7 +591,6 @@ schema_to_display_lookup <- data.frame(schema_name, display_name)
           folder_list <- syn_store$getStorageDatasetsInProject(synStore_obj, project_synID)
 folders_namedList <- setNames(as.list(folder_df$id), folder_df$name)
 folder_df <- syn_tableQuery(sprintf("select name, id from %s where type = 'folder' and projectId = '%s'", config$main_fileview, project_synID))$asDataFrame()
-          for (i in seq_along(folder_list)) {
           
           folder_synID <- folders_namedList[[selected_folder]]
           validate(
